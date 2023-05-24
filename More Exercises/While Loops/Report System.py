0691d7af8df293cc37e2bb30bd6c1c76095e4890
Ivan Tsekov <ivantsekov16@gmail.com>
2023-05-25 01:59:18 +0300

    Report System Updated

--- More Exercises/While Loops/Report System.py
+++ More Exercises/While Loops/Report System.py
@@ -5,32 +5,35 @@ cash = 0
 card = 0
 cash_count = 0
 card_count = 0
+command = ""
 
-while True:
+while command != "End" and total < needed_sum:
+    command = input()
+    if command != "End":
+        money = int(command)
+    else:
+        continue
     method += 1
-    money = input()
-    if money != "End":
-        money = int(money)
-        if method % 2 != 0 and money >= 100:
-            print("Error in transaction!")
-        elif method % 2 == 0 and money <= 10:
-            print("Error in transaction!")
-        elif method % 2 != 0 and money <= 100:
-            print("Product sold!")
+    isCash = method % 2 == 1 and money <= 100
+    isCard = method % 2 == 0 and money >= 10
+
+    if isCard or isCash:
+        print("Product sold!")
+        total += money
+        if isCash:
             cash += money
-            total += money
             cash_count += 1
-        elif method % 2 == 0 and money >= 100:
-            print("Product sold!")
+        elif isCard:
             card += money
-            total += money
             card_count += 1
-    if total >= needed_sum:
-        print(f"Average CS: {cash / cash_count:.2f}")
-        print(f"Average CC: {card / card_count:.2f}")
-        break
-    elif total < needed_sum and money == "End":
-        print("Failed to collect required money for charity.")
-        break
+    else:
+        print("Error in transaction!")
 
 
+
+
+if total >= needed_sum:
+    print(f"Average CS: {cash / cash_count:.2f}")
+    print(f"Average CC: {card / card_count:.2f}")
+else:
+    print("Failed to collect required money for charity.")
